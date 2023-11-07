1f8fc9ff53391e7d6aff8b3e747597f958c6d477
TEmilka <134950198+TEmilka@users.noreply.github.com>
2023-11-07 22:19:46 +0100

    Update ConsoleApplication1.cpp

--- ZADANIE_2/ConsoleApplication1.cpp
+++ ZADANIE_2/ConsoleApplication1.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main() 
 {
+    Matrix matrix(10, 10);
+    matrix.rand();
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            cout << matrix.getValue(i, j) << " ";
+        }
+        cout << std::endl;
+    }
+    matrix.insertValue(0, 0, -3);
+    cout << matrix.getValue(0, 0) << std::endl;
+    cout << "Error state: " << matrix.fail() << std::endl;
+    matrix.insertValue(-9, -123, 23);
+    cout << "Error state: " << matrix.fail() << std::endl;
+    matrix.resetFail();
+    cout << "Error state: " << matrix.fail() << std::endl;
 
-
+    return 0;
 }
